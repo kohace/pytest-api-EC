ab1c63a899cb896930df056d9381c1f18a9ae7a2
kohace <1172231902@qq.com>
2024-11-01 17:05:53 +0800

    jenkins提交

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import shutil
 import pytest
 
 if __name__ == '__main__':
-    pytest.main()
+    pytest.main(['--junitxml=./report/results.xml'])
     shutil.copy('./environment.xml', './report/temp')
     os.system(f'allure serve ./report/temp')
     # pytest.main(['-vs', './testcase', '-m', 'maoyan'])
